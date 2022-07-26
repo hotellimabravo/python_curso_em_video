e4fb34752a1e689516fa0a6cbae63021dc997aae
hotellimabravo <hugobalttazar@gmail.com>
2022-07-26 16:34:30 -0300

    chore: exercicio 63

---
+++ mundo2/pythonExercicios/ex063.py
@@ -0,0 +1,17 @@
+#  Sequencia Fibonacci
+print('~'*40)
+print('{:^40}'.format('Sequência de Fibonacci'))
+print('~'*40)
+n = int(input('Quantos termos você quer mostrar?: '))
+t1 = 0
+t2 = 1
+print('~'*40)
+print('{} -> {}'.format(t1, t2), end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print(' -> {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' -> FIM')
