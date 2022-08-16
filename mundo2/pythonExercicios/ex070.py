9d6c4199584e2c77fda3aac1f9261145e01c5b25
hotellimabravo <hugobalttazar@gmail.com>
2022-08-16 14:55:04 -0300

    chore: exercicio 71

--- mundo2/pythonExercicios/ex070.py
+++ mundo2/pythonExercicios/ex070.py
@@ -8,15 +8,11 @@ while True:
     print(f'Produto: {contador}')
     pdpName = str(input('Nome do produto: ')).strip().upper()
     pdpPrice = float(input('Valor do Produto: R$'))
-    answer = str(input('Deseja cadastrar outro produto? [Sim/Não]: ')).strip().upper()[0]
+    answer = ' '
+    while answer not in 'SN':
+        answer = str(input('Deseja cadastrar outro produto? [Sim/Não]: ')).strip().upper()[0]
     tot = tot + pdpPrice
-    if contador == 1:
-        barato = pdpPrice
-        caro = pdpPrice
-        cheapName = pdpName
-    if pdpPrice > caro:
-        caro = pdpPrice
-    if pdpPrice < caro:
+    if contador == 1 or pdpPrice < barato:
         barato = pdpPrice
         cheapName = pdpName
     if pdpPrice > 1000:
@@ -28,4 +24,4 @@ print('{:^30}'.format('PROGRAMA FINALIZADO!'))
 print('*' * 30)
 print(f'Total Gasto: R${tot}')
 print(f'Na sua compra {totMil} produtos custam mais de Mil Reais')
-print(f'O produto mais barato da sua compra é: {cheapName}')
+print(f'O produto mais barato da sua compra é: {cheapName} que custa R${barato}')
