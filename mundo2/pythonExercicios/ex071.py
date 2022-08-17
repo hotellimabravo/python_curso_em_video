76e8289ba39a7a4bb7ab79fc3f69423754392225
hotellimabravo <hugobalttazar@gmail.com>
2022-08-16 22:53:22 -0300

    chore: exercicio 71 correcao

--- mundo2/pythonExercicios/ex071.py
+++ mundo2/pythonExercicios/ex071.py
@@ -1,26 +1,33 @@
-print('=' * 30)
-print('{:^30}'.format('UDI BANK S/A'))
-print('=' * 30)
-valor = int(input('Valor do saque: R$'))
-total = valor
-ced = 50
-totced = 0
-while True:
-    if total >= ced:
-        total -= ced
-        totced += 1
-    else:
-        if totced > 0:
-            print(f'Total de {totced} cedulas de R${ced}')
-        if ced == 50:
-            ced = 20
-        elif ced == 20:
-            ced = 10
-        elif ced == 10:
-            ced = 1
-        totced = 0
-        if total == 0:
-            break
-print('=' * 30)
-print('{:^30}'.format('VOLTE SEMPRE!'))
-print('=' * 30)
+# print('=' * 30)
+# print('{:^30}'.format('UDI BANK S/A'))
+# print('=' * 30)
+# valor = int(input('Valor do saque: R$'))
+# total = valor
+# ced = 100
+# totced = 0
+# while True:
+#     if total >= ced:
+#         total -= ced
+#         totced += 1
+#     else:
+#         if totced > 0:
+#             print(f'Total de {totced} cedulas de R${ced}')
+#         elif ced == 100:
+#             ced = 50
+#         elif ced == 50:
+#             ced = 20
+#         elif ced == 20:
+#             ced = 10
+#         elif ced == 10:
+#             ced = 5
+#         elif ced == 5:
+#             ced = 1
+#         totced = 0
+#         if total == 0:
+#             break
+# print('=' * 30)
+# print('{:^30}'.format('VOLTE SEMPRE!'))
+# print('=' * 30)
+
+for c in range(0,5):
+    print(c)
