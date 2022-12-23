86f4f06e04c5975da063604649a936672821424e
hotellimabravo <hugobalttazar@gmail.com>
2022-12-23 14:15:47 -0600

    Exercicios 79 a 81

--- mundo3/pythonExercicios/ex079.py
+++ mundo3/pythonExercicios/ex079.py
@@ -1,6 +1,16 @@
 num = []
+resp = 'S'
 
-while True:
-    num.append(int(input('Digite um número: ')))
-
-print(num)
+while resp == 'S':
+    n = (int(input('Digite um número: ')))
+    if n not in num:
+        num.append(n)
+        print('Valor adicionado com sucesso')
+    else:
+        print('Valores repetidos não serão adicionados...')
+    resp = str(input('Deseja continuar [S|N]? : ')).upper().strip()[0]
+    if resp == 'N':
+        break
+print('=-' * 35)
+print(f'Essa é a lista >>> {sorted(num)} <<<')
+print('=-' * 35)
