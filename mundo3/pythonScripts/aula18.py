254a75206d1ce1213d2a011effe3174593c61e00
hotellimabravo <hugobalttazar@gmail.com>
2023-01-25 21:57:15 -0300

    changes class 18

--- mundo3/pythonScripts/aula18.py
+++ mundo3/pythonScripts/aula18.py
@@ -1,1 +1,37 @@
-teste =
+# teste = list()
+# teste.append('Gustavo')
+# teste.append(40)
+# galera = list()
+# galera.append(teste[:])
+# teste[0] = 'Maria'
+# teste[1] = 22
+# galera.append(teste[:])
+# print(galera)
+
+# FIM
+
+# galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+# # print(galera[3][1])
+# for p in galera:
+#     print(f'{p[0]} tem {p[1]} anos de idade.')
+
+# FIM
+
+# galera = list()
+# dado = list()
+# totmai = totmen = 0
+# for c in range(0, 3):
+#     dado.append(str(input('Nome: ')))
+#     dado.append(int(input('idade: ')))
+#     galera.append(dado[:])
+#     dado.clear()
+# for p in galera:
+#     if p[1] >= 21:
+#         print(f'{p[0]} é maior de idade.')
+#         totmai += 1
+#     else:
+#         print(f'{p[0]} é menor de idade.')
+#         totmen += 1
+#
+# print(f'temos {totmai} maiores e {totmen} menores de idade.')
+# print(galera)
